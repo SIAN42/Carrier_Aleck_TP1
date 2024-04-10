a7b9e88edb83d6a8fe00ec595692376b5b938a45
Aleck Carrier <aleck.carrier@gmail.com>
2024-04-10 16:06:35 -0400

    avancement

--- Assets/Scripts/Nuage_ObjetVolant.cs
+++ Assets/Scripts/Nuage_ObjetVolant.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class Nuage_ObjetVolant : MonoBehaviour
 {
 
-    [SerializeField] private float _vitesse = 1f;
+    [SerializeField] private float vitesse = 1;
 
-    [SerializeField] private float _direction = 1f;
+    //[SerializeField] private string _direction = "foward";
 
     [SerializeField] private float repeatTime = 0.5f;
 
@@ -13,7 +13,7 @@ public class Nuage_ObjetVolant : MonoBehaviour
     void Update()
     {
         
-        transform.Translate(Vector3._direction * _vitesse * Time.deltaTime)
+        transform.Translate(Vector3.foward * vitesse * Time.deltaTime)
 
     }
 }
